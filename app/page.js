f1d1cf924d3087c5978c4edf5ffed4ead8d27b98
Your Name <sitholekamogelo07@gmail.com>
2023-12-23 15:37:44 +0200

    ts eeror

--- app/page.js
+++ app/page.js
@@ -17,9 +17,9 @@ export default function Home() {
       <div className="flex flex-col  items-center  space-y-6 ">
         <h1>hey, I'm Kamogelo 👋</h1>
         <h1>
-          I'm Patryk, a product designer and design system specialist from
-          Poland with over 9 years of experience focusing on user experience and
-          design systems to creating a user-centered design in SaaS products.
+          Im Patryk, a product designer and design system specialist from Poland
+          with over 9 years of experience focusing on user experience and design
+          systems to creating a user-centered design in SaaS products.
           Co-creator at Tetrisly one of the most popular starter kits for design
           systems. I had the opportunity to work with Phenom.com, Bidroom.com or
           Perfops.net but also with many startups from around the world as a
